0e492f2215a322cca9ee6987bc82744f4cc96ac8
Robert Levchenko <ro.le.00@mail.ru>
2022-06-17 18:34:29 +0300

    Before the first review with marks

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -111,7 +111,7 @@ CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
     }
-} 
+}
 
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
